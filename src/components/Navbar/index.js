7091b88b3cf8f74ad992504ded7680bc86cd6280
Emerson Silva <emerson_jdss@hotmail.com>
2020-11-22 06:45:50 -0300

    adjust padding on screen

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import {
-  Navbar,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-} from 'reactstrap';
+import React from "react";
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
 
 const NavbarComponent = () => {
   return (
-      <div>
-        <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">Portum</NavbarBrand>
-          <Nav className="ml-auto" navbar>
-            <NavItem>
-              <NavLink href="#">Link</NavLink>
-            </NavItem>
-          </Nav>
-        </Navbar>
-      </div>
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Portum</NavbarBrand>
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <NavLink href="#">Configurações</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#">Sair</NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
+    </div>
   );
-}
+};
 
 export default NavbarComponent;
--- src/components/TableComponent/index.js
+++ src/components/TableComponent/index.js
@@ -14,16 +14,14 @@ moment.locale("pt-br");
 //   .format('D[ day(s)] H[ hour(s)] m[ minute(s)] s[ second(s) ago.]')
 // );
 
-
 function diffDays(init, finish) {
-  let dateInit = init
+  let dateInit = init;
   var temp = dateInit.split(" ");
   dateInit = temp[0].split("/").reverse().join("-") + " " + temp[1];
 
   let dateFinish = finish;
   var temp = dateFinish.split(" ");
-  dateFinish =
-    temp[0].split("/").reverse().join("-") + " " + temp[1];
+  dateFinish = temp[0].split("/").reverse().join("-") + " " + temp[1];
 
   // console.log(
   //   estadia["Atracação Prevista"],
@@ -41,7 +39,7 @@ function diffDays(init, finish) {
   // console.log(diffDays);
   // console.log("============");
 
-  return diffDays + 1
+  return diffDays + 1;
 }
 
 export default function TableComponent(props) {
@@ -50,7 +48,6 @@ export default function TableComponent(props) {
   console.log("TableComponent", estadias);
 
   return (
-
     <Fragment>
       <h3>Atracações da Semana</h3>
       <Table size="sm">
@@ -109,17 +106,17 @@ export default function TableComponent(props) {
             return (
               <tr>
                 {/* <th>{estadia["Número do DUV"].slice(0, 10) + "..."}</th> */}
-                <th>{estadia["Bandeira da Embarcação"]}</th>
-                <th>Status</th>
-                <th>{estadia["Atracação Prevista"]}</th>
-                <th>{estadia["Atracação Efetiva"]}</th>
-                <th>{estadia["Desatracação Efetiva"]}</th>
-                <th>{estadia["Desatracação Prevista"]}</th>
-                <th>{diffDaysPrev} Dias</th>
-                <th>{diffDaysEfet} Dias</th>
-                <th>{(diffDaysEfet-diffDaysPrev)}</th>
-                <th>{estadia["SLA Atracação Limit"]}</th>
-                <th>{estadia["SLA Desatracação Limit"]}</th>
+                <td>{estadia["Bandeira da Embarcação"]}</td>
+                <td>Status</td>
+                <td>{estadia["Atracação Prevista"]}</td>
+                <td>{estadia["Atracação Efetiva"]}</td>
+                <td>{estadia["Desatracação Efetiva"]}</td>
+                <td>{estadia["Desatracação Prevista"]}</td>
+                <td>{diffDaysPrev} Dias</td>
+                <td>{diffDaysEfet} Dias</td>
+                <td>{diffDaysEfet - diffDaysPrev}</td>
+                <td>{estadia["SLA Atracação Limit"]}</td>
+                <td>{estadia["SLA Desatracação Limit"]}</td>
               </tr>
             );
           })}
