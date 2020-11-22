cd66a91bc8012d3c60a502a4859f925633b0d4e6
Emerson Silva <emerson_jdss@hotmail.com>
2020-11-22 12:39:59 -0300

    remove unused data

--- src/components/TabComponent/index.js
+++ src/components/TabComponent/index.js
@@ -360,30 +360,6 @@ export default function TabComponent(props) {
             </Col>
           </Row>
         </TabPane>
-        <TabPane tabId="2">
-          <Row>
-            <Col sm="6">
-              <Card body>
-                <CardTitle>Special Title Treatment</CardTitle>
-                <CardText>
-                  With supporting text below as a natural lead-in to additional
-                  content.
-                </CardText>
-                <Button>Go somewhere</Button>
-              </Card>
-            </Col>
-            <Col sm="6">
-              <Card body>
-                <CardTitle>Special Title Treatment</CardTitle>
-                <CardText>
-                  With supporting text below as a natural lead-in to additional
-                  content.
-                </CardText>
-                <Button>Go somewhere</Button>
-              </Card>
-            </Col>
-          </Row>
-        </TabPane>
       </TabContent>
     </div>
   );
