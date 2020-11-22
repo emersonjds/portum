6eb273e64ea3a886097ef0767666bd33018bc5c7
Emerson Silva <emerson_jdss@hotmail.com>
2020-11-22 10:13:20 -0300

    config card on Modal

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -10,7 +10,20 @@ import {
   ModalBody,
   ModalFooter,
   Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText,
 } from "reactstrap";
+import {
+  VesselContainer,
+  LeftAreaContainer,
+  RightAreaContainer,
+  MiddleAreaContainer,
+} from "./styles";
+
+import vessel from "../../assets/img/vessel.jpg";
 
 const NavbarComponent = () => {
   const [modal, setModal] = useState(false);
@@ -22,14 +35,40 @@ const NavbarComponent = () => {
       <Modal isOpen={modal} toggle={toggle}>
         <ModalHeader toggle={toggle}>Ajustar SLA</ModalHeader>
         <ModalBody>
-          <h1>Emerson</h1>
+          <div>
+            <VesselContainer>
+              <LeftAreaContainer>
+                <img src={vessel} height="100%" width="100%" />
+              </LeftAreaContainer>
+              <MiddleAreaContainer>
+                <span style={{ fontWeight: "bold" }}>MSC Rossvelt</span> <br />
+                <span>Tipo: graneleiro</span>
+                <br />
+                <span>Carga: 180 tn</span>
+              </MiddleAreaContainer>
+              <RightAreaContainer>
+                <Form>
+                  <FormGroup>
+                    <Label for="exampleEmail" style={{ fontWeight: "bold" }}>
+                      SLA Atracação
+                    </Label>
+                    <Input
+                      plaintext
+                      value="2 dias"
+                      style={{ textAlign: "right", fontWeight: "bold" }}
+                    />
+                  </FormGroup>
+                </Form>
+              </RightAreaContainer>
+            </VesselContainer>
+            <Button color="info" size="sm" style={{ float: "right" }}>
+              Editar
+            </Button>{" "}
+          </div>
         </ModalBody>
         <ModalFooter>
-          <Button color="primary" onClick={toggle}>
-            Do Something
-          </Button>{" "}
-          <Button color="secondary" onClick={toggle}>
-            Cancel
+          <Button color="success" onClick={toggle}>
+            Fechar
           </Button>
         </ModalFooter>
       </Modal>
